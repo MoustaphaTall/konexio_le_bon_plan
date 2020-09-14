6e792a7ed246206ea2ef46a103e6ebde40291064
MoustaphaTall <65893882+MoustaphaTall@users.noreply.github.com>
2020-09-14 16:54:54 +0200

    first commit - created folder structure

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const exphbs = require('express-handlebars');
+const expressSession = require('express-session');
+const MongoStore = require('connect-mongo')(expressSession);
+const mongoose = require('mongoose');
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+
+const port = process.env.PORT || 3000;
+
+mongoose.connect(
+	process.env.MONGODB_URI || 'mongodb://localhost:27017/le_bon_plan',
+	{
+		useNewUrlParser: true,
+		useCreateIndex: true,
+		useUnifiedTopology: true,
+	},
+	(err) => {
+		if (err !== null) {
+			console.log('Error connecting to DB', err);
+			return;
+		}
+
+		console.log('DB successfully connected');
+	}
+);
+
+const app = express();
+
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get('/', (req, res) => {
+	res.render('home');
+});
+
+app.listen(port, () => {
+	console.log(`Server started on port ${port}`);
+});
