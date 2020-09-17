75e2a8c6a2e98c4a278a1f161f3c7ae70781f51c
unknown <adeline.sire@gmail.com>
2020-09-17 16:00:22 +0200

    cities

--- controllers/city_products.js
+++ controllers/city_products.js
@@ -1,9 +1,18 @@
 const express = require('express');
+const { Product } = require('../models');
 const router = express.Router();
+
 
 // route pour récuperer tout les produits qui sont dans la même ville
 router.get('/:city', (req, res) => {
-	// à faire
+	const city = req.params.city;
+	Product.find({city}, (err, productsDb) => {
+		const products = productsDb.map((product) => product.toObject());
+		console.log(products);
+		res.render('city', {products: products});
+	});
 });
 
 module.exports = router;
+
+
