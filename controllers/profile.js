8798f5ae2badb16f6f1bde29fbe72c7aad9a8f1c
MoustaphaTall <65893882+MoustaphaTall@users.noreply.github.com>
2020-09-17 16:03:15 +0200

    add authenticated check to access profile

--- controllers/profile.js
+++ controllers/profile.js
@@ -4,13 +4,18 @@ const router = express.Router();
 router.get('/', (req, res) => {
 	console.log('GET /profile');
 	// console.log('/profile req.user', req.user);
-	const { username, surname, firstname, userPic } = req.user;
-	res.render('profile', {
-		username,
-		surname,
-		firstname,
-		userPic,
-	});
+
+	if (req.isAuthenticated() === true) {
+		const { username, surname, firstname, userPic } = req.user;
+		res.render('profile', {
+			username,
+			surname,
+			firstname,
+			userPic,
+		});
+	} else {
+		res.redirect('/signup');
+	}
 });
 
 module.exports = router;
