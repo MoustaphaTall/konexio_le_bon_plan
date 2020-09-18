e2e2b385326a5a8e03b75281188154a8f05c0f3c
ines-ux <65893945+ines-ux@users.noreply.github.com>
2020-09-18 11:40:10 +0200

    problem push

--- controllers/products.js
+++ controllers/products.js
@@ -3,7 +3,22 @@ const router = express.Router();
 
 // route pour récuperer un produit par son id
 router.get('/:id', (req, res) => {
-	// à faire
+	const id = req.params.id;
+
+	ProductModel.find({ _id: id }, (err, product) => {
+		if (!err) {
+			console.log(product);
+			console.log(product.name);
+			res.render('product', {
+				name: product[0].name,
+				price: product[0].price,
+				description: product[0].description,
+				city: product[0].city,
+				images: product[0].images
+			});
+		}
+		res.re;
+	});
 });
 
 module.exports = router;
